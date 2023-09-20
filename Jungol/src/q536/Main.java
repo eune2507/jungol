f8e3a96f60dcdef2f597f881cca6240c70ff2fa6
21-manic-001g <21-manic-001g@21-manic-001>
2023-09-20 16:14:58 +0900

    반복문 수정

--- Jungol/src/q536/Main.java
+++ Jungol/src/q536/Main.java
@@ -3,13 +3,9 @@ package q536;
 public class Main {
 	public static void main(String[] args) {
 		int a = 1 ;
-		boolean b = true;
-		while (b) {
+		while (a < 16) {
 			System.out.print(a + " ");
 			a += 1;
-			if (a == 16) {
-				b = false;
-			}
 		}
 	}
 }
--- Jungol/src/q536/Main2.java
+++ Jungol/src/q536/Main2.java
@@ -2,10 +2,8 @@ package q536;
 
 public class Main2 {
 	public static void main(String[] args) {
-		int a = 1;
-		for (a=1; a<16;a++) {
-			System.out.print(a+ " ");
+		for (int a = 1; a < 16; a++) {
+			System.out.print(a + " ");
 		}
-		
-}
+	}
 }
