d212b7462f2cda6212f7d937d7fdc15f6fa1c608
21-manic-001g <21-manic-001g@21-manic-001>
2023-09-20 15:49:23 +0900

    9006, 528 풀이

---
+++ Jungol/src/q9006/Main.java
@@ -0,0 +1,12 @@
+package q9006;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("수를 출력할 때는 따옴표를 생략합니다.");
+		System.out.println(123);
+		System.out.print("6.500000");
+
+	}
+
+}
