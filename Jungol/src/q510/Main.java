2d0a4401f9e68c3610fd4863c94dcf13f55b7c15
21-manic-001g <21-manic-001g@21-manic-001>
2023-09-20 13:36:02 +0900

    a 변수값 변경

--- Jungol/src/q510/Main.java
+++ Jungol/src/q510/Main.java
@@ -1,7 +1,6 @@
 package q510;
 
 public class Main {
-
 	public static void main(String[] args) {
 		int a = -1;
 		int b = 100; 
@@ -9,6 +8,8 @@ public class Main {
 		System.out.println(a);
 		System.out.println(b);
 
+		a = 99;
+		System.out.println(a);
+		System.out.println(b);
 	}
-
 }
