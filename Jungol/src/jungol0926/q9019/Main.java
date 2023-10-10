a733e4a8f6983fc0724b997968619cc011b9832c
21-manic-001g <21-manic-001g@21-manic-001>
2023-10-10 17:44:05 +0900

    1010

--- Jungol/src/jungol0926/q9019/Main.java
+++ Jungol/src/jungol0926/q9019/Main.java
@@ -7,18 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("5개의 수를 입력하시오. ");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
-		int e = sc.nextInt();
+		int a = sc.nextInt()+3;
+		int b = sc.nextInt()-3;
+		int c = sc.nextInt()*3;
+		int d = sc.nextInt()/3;
+		int e = sc.nextInt()%3;
 		sc.close();
 	
-		System.out.print(a+3+" ");
-		System.out.print(b-3+" ");
-		System.out.print(c*3+" ");
-		System.out.print(d/3+" ");
-		System.out.print(e%3);
+		System.out.printf("%d %d %d %d %d",a,b,c,d,e);
 
 	}
 
